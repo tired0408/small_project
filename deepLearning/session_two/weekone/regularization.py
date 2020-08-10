1694af91475d01703c6165f2e8567e1352deac54
hujt <89085657@qq.com>
2020-08-10 22:10:27 +0800

    修正导包

--- deepLearning/session_two/weekone/regularization.py
+++ deepLearning/session_two/weekone/regularization.py
@@ -3,8 +3,8 @@
 # import packages
 import numpy as np
 import matplotlib.pyplot as plt
-from data.reg_utils import sigmoid, relu, plot_decision_boundary, initialize_parameters, load_2D_dataset, predict_dec
-from data.reg_utils import compute_cost, predict, forward_propagation, backward_propagation, update_parameters
+from deepLearning.session_two.weekone.data.reg_utils import sigmoid, relu, plot_decision_boundary, initialize_parameters, load_2D_dataset, predict_dec
+from deepLearning.session_two.weekone.data.reg_utils import compute_cost, predict, forward_propagation, backward_propagation, update_parameters
 import sklearn
 import sklearn.datasets
 import scipy.io
