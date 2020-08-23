2fef455cec33a438befb61b728f590b3addbff03
hujt <89085657@qq.com>
2020-08-23 14:04:49 +0800

    change the file path

--- deepLearning/session_four/weektwo/keras_tutorial.py
+++ deepLearning/session_four/weektwo/keras_tutorial.py
@@ -1,19 +1,12 @@
 # 参考地址：https://blog.csdn.net/qq_21997625/article/details/80984004
-import numpy as np
-from keras import layers
 from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
-from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
+from keras.layers import MaxPooling2D
 from keras.models import Model
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input
-import pydot
-from IPython.display import SVG
-from keras.utils.vis_utils import model_to_dot
-from keras.utils import plot_model
-from deepLearning.session_four.weektwo.kt_utils import *
+from deepLearning.session_four.weektwo.utils.kt_utils import *
 import keras.backend as K
 K.set_image_data_format('channels_last')
-import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 
 X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
--- deepLearning/session_four/weektwo/residual_networks.py
+++ deepLearning/session_four/weektwo/residual_networks.py
@@ -1,20 +1,8 @@
 # 参考地址：https://blog.csdn.net/qq_21997625/article/details/80990977
-import numpy as np
-from keras import layers
-from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
-from keras.models import Model, load_model
-from keras.preprocessing import image
-from keras.utils import layer_utils
-from keras.utils.data_utils import get_file
-from keras.applications.imagenet_utils import preprocess_input
-import pydot
-from IPython.display import SVG
-from keras.utils.vis_utils import model_to_dot
-from keras.utils import plot_model
-from deepLearning.session_four.weektwo.resnets_utils import *
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D
+from keras.models import Model
+from deepLearning.session_four.weektwo.utils.resnets_utils import *
 from keras.initializers import glorot_uniform
-import scipy.misc
-from matplotlib.pyplot import imshow
 import keras.backend as K
 K.set_image_data_format('channels_last')
 K.set_learning_phase(1)
